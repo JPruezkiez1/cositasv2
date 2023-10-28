e92cf689fd32cfe7adbc928ef8ea65a5b286360e
JPruezkiez1 <jpruezkiez@icloud.com>
2023-10-27 19:56:38 -0500

    IMG fix

--- src/Utility/ImageCard/ImageCard.js
+++ src/Utility/ImageCard/ImageCard.js
@@ -6,7 +6,6 @@ export default function ImageCard({ imagedata, customStyle, handleOpen, setSelec
         setSelectedImage(item.image);
     };
     return (
-
         <>
             {imagedata.map((item) => (
                 <div onClick={() => handleImageClick(item)} className="photo_place01" style={customStyle} key={item.id} >
@@ -14,7 +13,6 @@ export default function ImageCard({ imagedata, customStyle, handleOpen, setSelec
                     <p className='legend_01'>{item.name}</p>
                 </div>
             ))}
-
         </>
     )
 }
